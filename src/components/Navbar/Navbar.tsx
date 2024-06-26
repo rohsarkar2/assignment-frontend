9705e6b6dba7feb220155b3b81ead7972066b80f
Rohan Sarkar <rohan.sarkar@qbent.in>
2024-06-26 17:41:47 +0530

    small changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Menubar } from "primereact/menubar";
 import { Button } from "primereact/button";
 import { Colors } from "../../configs/Colors";
--- src/components/last-component/LastComponent.tsx
+++ src/components/last-component/LastComponent.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./LastComponent.css";
 
 export const LastComponent = () => {
